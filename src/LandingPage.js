275672776359b4970cb7cbcabc400bb2c41b7839
Byung San Kwak <kwak@Byungs-iMac.local>
2024-01-06 09:54:36 -0600

    initial

---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+    return (
+        <div className="landing-page">
+            <h1>Welcome to Our Kendo Club</h1>
+            <p>Join our community to learn and practice the art of Kendo.</p>
+        </div>
+    );
+}
+
+export default LandingPage;
